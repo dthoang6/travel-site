32d4be355fdc99128be15f0036ec0a0c36b9cd18
Dat Hoang <dthoang6@gmail.com>
2022-12-28 14:28:50 -0800

    improve performance by using throttle to call function, and debounce

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,7 +4,15 @@ import RevealOnScroll from "./modules/RevealOnScroll";
 
 let mobileMenu = new MobileMenu();
 
-let revealOnScroll = new RevealOnScroll();
+/* create one new instance of the reveal on scroll class
+- and in these parentheses, let's give it a DOM selector
+- selector that selects these feature items
+- and then we can create another new instance of the same class and
+- give it a selector that selects these testimonial items
+- then we just need to go into the class and make it flexible */
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"));
+new RevealOnScroll(document.querySelectorAll(".testimonial"));
 
 if (module.hot) {
   module.hot.accept();
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,37 +1,67 @@
-import throttle from "lodash/throttle";
+import throttle from "lodash/throttle"; /* run our function every 200ms continuously as we are scrolling */
+import debounce from "lodash/debounce"; /* do something once after we finally stopped resizing */
 
 class RevealOnScroll {
-  constructor() {
+  constructor(els) {
     this.itemsToReveal = document.querySelectorAll(".feature-item");
+    this.browserHeight = window.innerHeight;
     this.hideInitially();
+    /* the web browser is calling our function for every single pixel we scroll
+    which is inefficient, we will throttle it to call for every 200ms as we scroll it.
+    */
     this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
   events() {
-    /* listening for scroll events and add the visible class to the hidden
-    elements at precisely the right moment when they're scrolled to */
-    window.addEventListener("scroll", () => {
-      /* as we are scrolling, we want to check to see if any of our four feature
-      item to reveal elements have been scrolled to yet */
-      this.itemsToReveal.forEach(el => {
-        /* run a calculation once for each of those items
-        in a separate method and call that method here
-        */
+    window.addEventListener("scroll", this.scrollThrottle);
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        console.log("Resize just ran");
+        this.browserHeight = window.innerHeight;
+      })
+    );
+  }
+
+  calcCaller() {
+    this.itemsToReveal.forEach(el => {
+      if (el.isRevealed == false) {
         this.calculateIfScrolledTo(el);
-      });
+      }
     });
   }
 
   calculateIfScrolledTo(el) {
-    let scrollPercent = (el.getBoundingClientRect().top / window.innerHeight) * 100;
-    if (scrollPercent < 75) {
-      el.classList.add("reveal-item--is-visible");
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      /* if the top of page of an item needs to be revealed has not even
+      crossed the bottom edge of the browsers viewport yet, do not calculate. */
+      console.log("Element was calculated.");
+      let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100;
+      if (scrollPercent < 75) {
+        el.classList.add("reveal-item--is-visible");
+        /* set property to true to use if an element is revealed to stop running calculation*/
+        el.isRevealed = true;
+        /* set up an if statement so that  once the final item that needs to
+        revealed, let's completely remove the scroll event listener.*/
+        if (el.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottles);
+        }
+      }
     }
   }
 
   hideInitially() {
-    this.itemsToReveal.forEach(el => el.classList.add("reveal-item"));
+    this.itemsToReveal.forEach(el => {
+      el.classList.add("reveal-item");
+      /* add a new property "isRevealed" to each feature item and
+      by default set it to false, then once we actually calculate that it should be visible,
+      we can set it to true.*/
+      el.isRevealed = false;
+    });
+    /* add a new property "isLastItem" to setup if statement to check to remove the
+    scroll event listener*/
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
   }
 }
 
