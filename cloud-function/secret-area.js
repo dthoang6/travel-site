88def7b0f47b2126db0ea15a31dbd6bee511a6ce
Dat Hoang <dthoang6@gmail.com>
2023-01-09 21:30:52 -0800

    try again to create cloud function

--- cloud-function/secret-area.js
+++ cloud-function/secret-area.js
@@ -1,7 +1,12 @@
 exports.handler = function (event, context, callback) {
+  /* you would actually do something before calling the callback
+  -talk to database
+  -tell a third party to send an email
+  -write an if statement to evaluate the incoming string of text
+  that a visitor sent with their requests.
+  */
   callback(null, {
     statusCode: 200,
     body: "Welcome to the super secret area."
   })
-  
 }
